8fd241a71d32b9c40ddc92f82bd48c7f28603b8d
aaronmendiola93 <icmaarongmendiola@gmail.com>
2023-11-08 11:45:46 -0600

    Se agregan User.service, User.Repository e User.controller

---
+++ src/main/java/com/gen/shuncosDB/service/UserService.java
@@ -0,0 +1,47 @@
+package com.gen.shuncosDB.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gen.shuncosDB.model.User;
+import com.gen.shuncosDB.repository.UserRepository;
+
+@Service
+
+
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+
+	//Get
+	public User getUserById(Long id){
+		return userRepository.findById(id).orElse(null);
+	}
+
+	//Post
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	//Put
+	public User updateUser(Long id, User userDetails) {
+		User user = userRepository.findById(id).orElse(null);
+		
+		user.setFirst_name(userDetails.getFirst_name());
+		user.setLast_name(userDetails.getLast_name());
+		user.setUsername(userDetails.getUsername());
+		user.setEmail(userDetails.getEmail());
+		user.setPassword(userDetails.getPassword());
+		//user.setIs_admin(userDetails.getIs_admin());
+		
+		return userRepository.save(user);
+		
+	}
+	
+	//Delete
+	public void deleteUser(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+}
