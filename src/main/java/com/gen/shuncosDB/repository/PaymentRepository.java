86843f3226f6809173742eccbef87d3175e1c282
Erickld <erickld.24@gmail.com>
2023-11-08 10:41:56 -0600

    ajustes api

--- src/main/java/com/gen/shuncosDB/repository/PaymentRepository.java
+++ src/main/java/com/gen/shuncosDB/repository/PaymentRepository.java
@@ -1,8 +1,8 @@
-package repository;
+package com.gen.shuncosDB.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import model.Payment;
+import com.gen.shuncosDB.model.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long>{
 
--- src/main/java/com/gen/shuncosDB/repository/ProductRepository.java
+++ src/main/java/com/gen/shuncosDB/repository/ProductRepository.java
@@ -1,8 +1,8 @@
-package repository;
+package com.gen.shuncosDB.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import model.Product;
+import com.gen.shuncosDB.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
